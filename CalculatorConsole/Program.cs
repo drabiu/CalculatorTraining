84e0c1a55520f4a9815574d696f40071c5727a03
mdrab <mateusz.drab@dfg.com>
2017-12-07 14:30:58 +0100

    ddd

--- CalculatorConsole/Program.cs
+++ CalculatorConsole/Program.cs
@@ -6,7 +6,7 @@ namespace CalculatorConsole
 	class Program
 	{
 		static void Main(string[] args)
-		{          
+		{
             Console.WriteLine("Podaj typ kalkulatora:");
             string enteredType = Console.ReadLine();
 
@@ -14,8 +14,10 @@ namespace CalculatorConsole
             {
                 Console.WriteLine("Podaj działanie:");
                 string enteredLine = Console.ReadLine();
+                if (enteredLine == "quit")
+                    break;
 
-                char operation = FindOperationInText(enteredLine);
+                char operation = SignConverter.FindOperationInText(enteredLine);
                 string[] values = enteredLine.Split(operation);
 
                 decimal enteredValue = int.Parse(values[0]);
@@ -32,42 +34,6 @@ namespace CalculatorConsole
                 Console.WriteLine("=");
                 Console.WriteLine(result);
             }
-        }
-
-        public static bool CheckIfSignIsNumber(char sign)
-        {
-            bool result = false;
-            char[] digits = new char[10] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-            foreach (char digit in digits)
-            {
-                if(sign == digit)
-                {
-                    result = true;
-                    break;
-                }
-                else
-                {
-                    result = false;
-                }
-            }
-
-            return result;
-        }
-
-        public static char FindOperationInText(string text)
-        {
-            char result = ' ';
-            foreach (char sign in text)
-            {
-                bool isNumber = CheckIfSignIsNumber(sign);
-                if (!isNumber)
-                {
-                    result = sign;
-                    break;
-                }
-            }
-
-            return result;
-        }
-	}
+        }     
+    }
 }
---
+++ CalculatorConsole/SignConverter.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace CalculatorConsole
+{
+    public static class SignConverter
+    {
+        private static bool CheckIfSignIsNumber(char sign)
+        {
+            bool result = false;
+            char[] digits = new char[10] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            foreach (char digit in digits)
+            {
+                if (sign == digit)
+                {
+                    result = true;
+                    break;
+                }
+                else
+                {
+                    result = false;
+                }
+            }
+
+            return result;
+        }
+
+        public static char FindOperationInText(string text)
+        {
+            char result = ' ';
+            foreach (char sign in text)
+            {
+                bool isNumber = CheckIfSignIsNumber(sign);
+                if (!isNumber)
+                {
+                    result = sign;
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        public static char FindOperationInTextParse(string text)
+        {
+            char result = ' ';
+            foreach (char sign in text)
+            {
+                try
+                {
+                    int.Parse(sign.ToString());
+                }
+                catch (FormatException ex)
+                {
+                    result = sign;
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        public static char FindOperationInTextTryParse(string text)
+        {
+            char result = ' ';
+            foreach (char sign in text)
+            {
+                int zmienna;
+                bool isNumber = int.TryParse(sign.ToString(), out zmienna);
+                if (!isNumber)
+                {
+                    result = sign;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
